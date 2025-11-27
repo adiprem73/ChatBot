35011336a3b7d7df85d8b0e17f70efb39a62652c
Aditya Prem <aditya.prem2003@gmail.com>
2025-11-27 21:22:12 +0530

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+// src/pages/Home.jsx
+
+import Chatbot from '../components/Chatbot';
+
+
+
+const Home = () => {
+  return (
+    <div className="absolute bg-black top-0 left-0 w-full h-full z-0">
+        <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-blue-500 rounded-full mix-blend-screen filter blur-[80px] opacity-20 animate-pulse"></div>
+        <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-purple-500 rounded-full mix-blend-screen filter blur-[80px] opacity-20 animate-pulse delay-1000"></div>
+      
+
+      <main>
+
+
+        <Chatbot />
+      </main>
+
+    </div>
+  );
+};
+
+export default Home;
